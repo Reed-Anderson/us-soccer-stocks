7204c51f50104a239683f445486595837798e2f4
Reed Anderson <reeda@ksu.edu>
2021-04-21 22:54:46 -0500

    buy confirmer

---
+++ src/components/modal.tsx
@@ -0,0 +1,48 @@
+import { Box, Button, Heading, Layer } from "grommet"
+import { FormClose } from "grommet-icons"
+import * as React from "react"
+import { COLORS } from "../misc/colors"
+import { GrowDiv } from "./simple-divs"
+
+/*******************************************************************************
+ *
+ * Modal
+ *
+ ******************************************************************************/
+
+/**
+ * Props for Modal
+ */
+ interface ModalProps {
+    closeFn: () => void
+    title: string
+}
+
+/**
+ * Modal Component
+ */
+const Modal: React.FC<ModalProps> = props => {
+    return (
+        <Layer onEsc={props.closeFn}>
+            <Box
+                align='center'
+                as='header'
+                background={COLORS["light-2"]}
+                direction='row'
+            >
+                <Heading level={5} margin="small">
+                    {props.title}
+                </Heading>
+                <GrowDiv />
+                <Button
+                    icon={<FormClose />}
+                    hoverIndicator={COLORS["light-4"]}
+                    onClick={props.closeFn}
+                />
+            </Box>
+            {props.children}
+        </Layer>
+    )
+}
+
+export default Modal
